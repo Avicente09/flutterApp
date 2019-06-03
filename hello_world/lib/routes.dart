e8e843fc76fe467d186caabaab4842b7a866b6e5
Laptop <kenyvicente09@gmail.com>
2019-06-03 17:21:44 -0600

    new

---
+++ hello_world/lib/routes.dart
@@ -0,0 +1,8 @@
+// import 'package:login_app/screens/home/home_screen.dart';
+// import 'package:login_app/screens/login/login_screen.dart';
+
+// final routes = {
+//   '/login': (BuildContext context) => new LoginScreen(),
+//   '/home': (BuildContext context) => new HomeScreen(),
+//   '/': (BuildContext context) => new LoginScreen(),
+// };
